3dd9f54fdb22e1e7e99909612bf8b21a9894c5f0
lazycat <w554870919@live.com>
2017-10-22 23:39:42 +0800

    测试图片预加载

--- scream/js/app.js
+++ scream/js/app.js
@@ -2,7 +2,8 @@ require.config({
     baseUrl: './js',
     paths: {
         'jquery': 'jquery.min',
-        'Swiper': '//s1.aomygodstatic.com/lib/Swiper/swiper.min',
+        'Swiper': 'Swiper/swiper.min',
+        'Preload': 'PreloadJS-0.6.2/lib/preloadjs-0.6.2.min'
     },
     shim: {
         'Swiper': {
--- scream/js/game.js
+++ scream/js/game.js
@@ -3,8 +3,49 @@ define(function(require) {
 	
 	var $ = require('jquery');
 	var Swiper = require('Swiper');
+	var Preload = require('Preload');
 	
 	var count = 0;
+
+	var manifest = [
+		"status1/arrow.png",
+		"status1/bg.png",
+		"status1/boy-light.png",
+		"status1/girl-light.png",
+		"status1/girl.png",
+		"status1/boy.png",
+		"status1/cloud.png",
+		"status1/coupon.png",
+		"status1/title.png"
+	];
+
+	var preload = new createjs.LoadQueue(true, "./img/");
+	preload.on('fileload', handleFileLoad);
+	preload.on('progress', handleOverallProgress);
+	preload.on("fileprogress", handleFileProgress);
+	preload.on('error', handleFileError);
+	preload.setMaxConnections(10);
+	
+	preload.loadManifest(manifest);
+	
+	function handleFileLoad() {
+		$(".preload-tips").hide();
+		$(".container").show();
+	}
+	
+	function handleOverallProgress(e) {
+		console.log(e);
+		console.log('progress', preload.progress);
+		$(".preload-progress").html(preload.progress * 100);
+	}
+	
+	function handleFileProgress(e) {
+		console.log(e);
+	}
+	
+	function handleFileError(event) {
+		$(".preload-tips").html("error" + JSON.stringify(event));
+	}
 
 	function countdown(a) {
 		$("time").html(a);
